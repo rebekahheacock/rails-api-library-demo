ec59831026e82b12bbd91f4e65df482564f8041d
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-22 09:56:24 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
   resources :examples, except: [:new, :edit]
+  # above line is same as
+  # resources :examples, only: [:index, :show, :create, :update, :destroy]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  get '/books', to: 'books#index'
+  get '/books/:id', to: 'books#show'
+  post '/books', to: 'books#create'
+  patch '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
+  # instead of above five routes could use
+  # resources :books, only: [:index, :show, :create, :update, :destroy]
+  # or
+  # resources :books, except: [:new, :edit]
+  # new and edit render the views; create and update deal w/database
 end
